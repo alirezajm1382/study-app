e279f11543130e55cbb31e6b465d575f4b709f94
Alireza Javadzadeh M. <alirezajm@hotmail.com>
2025-05-22 19:51:04 +0330

    refactor: standardize import quotes and simplify AuthLayoutIconButton logic + home page dvh refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-"use client";
-import SquaresClient from "@/components/bits/Squares/SquaresClient";
+'use client'
+import SquaresClient from '@/components/bits/Squares/SquaresClient'
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="absolute top-0 left-0 w-full h-full overflow-hidden -z-50 bg-black">
+    <div className='flex justify-center items-center h-dvh'>
+      <div className='absolute top-0 left-0 w-full h-full overflow-hidden -z-50 bg-black'>
         <SquaresClient
           speed={0.5}
           squareSize={40}
-          direction="diagonal"
-          borderColor="#333"
+          direction='diagonal'
+          borderColor='#333'
         />
       </div>
     </div>
-  );
+  )
 }
--- components/AuthLayoutIconButton.tsx
+++ components/AuthLayoutIconButton.tsx
@@ -1,35 +1,29 @@
-"use client";
-import React from "react";
+'use client'
+import React from 'react'
 
 // components
-import { Button } from "./ui/button";
+import { Button } from './ui/button'
 
 // hooks and utils
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter } from 'next/navigation'
 
 // icons and styles
-import { ArrowLeft, HomeIcon } from "lucide-react";
+import { ArrowLeft, HomeIcon } from 'lucide-react'
 
 const AuthLayoutIconButton: React.FunctionComponent = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  return pathname === "/auth" ? (
+  const router = useRouter()
+  const pathname = usePathname()
+  const redirect = pathname === '/auth' ? '/' : '/auth'
+  const ActionIcon = () => (pathname === '/auth' ? <HomeIcon /> : <ArrowLeft />)
+  return (
     <Button
-      size="icon"
-      onClick={() => router.replace("/")}
-      className="absolute top-0 left-0 text-white rounded-md border-1 border-white m-3"
+      size='icon'
+      onClick={() => router.replace(redirect)}
+      className='absolute top-0 left-0 text-white rounded-md border-1 border-white m-3'
     >
-      <HomeIcon />
+      <ActionIcon />
     </Button>
-  ) : (
-    <Button
-      size="icon"
-      onClick={() => router.back()}
-      className="absolute top-0 left-0 text-white rounded-md border-1 border-white m-3"
-    >
-      <ArrowLeft />
-    </Button>
-  );
-};
+  )
+}
 
-export default AuthLayoutIconButton;
+export default AuthLayoutIconButton
